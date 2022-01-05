c458530ad37e9ed4c7519d1bcadb3c2088642c1d
Geri <luga.geri@gmail.com>
2022-01-05 14:22:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import Header from "./components/Header";
+
+import initialEmails from "./data/emails";
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+    // Use initialEmails for state
+    const [emails, setEmails] = useState(initialEmails);
+
+    // function toggleRead(email) {
+    //     const newEmails = [...emails];
+    //     console.log( newEmails);
+    //     email.starred = !email.starred;
+    //     let finalEmails = newEmails.filter((mail) => {
+    //         return mail.id !== email.id;
+    //     });
+    //     console.log(finalEmails);
+
+    //     finalEmails.push(email);
+    //     setEmails(finalEmails);
+    // }
+    return (
+        <div className="app">
+            <Header />
+            <nav className="left-menu">
+                <ul className="inbox-list">
+                    <li
+                        className="item active"
+                        // onClick={() => {}}
+                    >
+                        <span className="label">Inbox</span>
+                        <span className="count">?</span>
+                    </li>
+                    <li
+                        className="item"
+                        // onClick={() => {}}
+                    >
+                        <span className="label">Starred</span>
+                        <span className="count">?</span>
+                    </li>
+
+                    <li className="item toggle">
+                        <label for="hide-read">Hide read</label>
+                        <input
+                            id="hide-read"
+                            type="checkbox"
+                            checked={false}
+                            // onChange={() => {}}
+                        />
+                    </li>
+                </ul>
+            </nav>
+            <main className="emails">
+                {emails.map((email) => {
+                    return (
+                        <section className="single-email">
+                            <input
+                                key={email.id}
+                                type="checkbox"
+                                // onChange={toggleRead}
+                                checked={email.read}
+                                name=""
+                                id=""
+                            />
+                            <input
+                                type="checkbox"
+                                checked={email.starred}
+                                name=""
+                                className="star"
+                            />
+                            <span className="sender">{email.sender}</span>
+                            <span className="title">{email.title}</span>
+                        </section>
+                    );
+                })}
+            </main>
+        </div>
+    );
+}
+
+export default App;
